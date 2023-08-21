5981c347f3505e8990ecc4a0d28ea94b60020920
Denys Redkin <redkindo@gmail.com>
2023-08-22 00:43:50 +0300

    th-26: * create instance notifications service

--- frontend/src/libs/packages/notification/notification.ts
+++ frontend/src/libs/packages/notification/notification.ts
@@ -1,8 +1,6 @@
-import { defaultNotificationOptions } from './libs/options/notification.options.default.js';
+import { DEFAULT_NOTIFICATION_OPTIONS } from './libs/options/notification.options.default.js';
 import { NotificationService } from './notification-api.js';
 
-const notification = NotificationService.getInstance(
-  defaultNotificationOptions,
-);
+const notification = new NotificationService(DEFAULT_NOTIFICATION_OPTIONS);
 
 export { notification };
