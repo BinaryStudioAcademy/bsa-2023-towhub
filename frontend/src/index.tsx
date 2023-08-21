a7994a2802133343e781a351d60e930cf5545f78
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-21 22:46:55 +0300

    th-25: + add Router component

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,45 +3,14 @@ import '~/assets/css/styles.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import {
-  App,
-  RouterProvider,
-  StoreProvider,
-} from '~/libs/components/components.js';
-import { AppRoute } from '~/libs/enums/enums.js';
+import { StoreProvider } from '~/libs/components/components.js';
+import { Router } from '~/libs/components/router/router.js';
 import { store } from '~/libs/packages/store/store.js';
-import { Auth } from '~/pages/auth/auth.js';
-import { NotFound } from '~/pages/not-found/not-found.js';
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
   <StrictMode>
     <StoreProvider store={store.instance}>
-      <RouterProvider
-        routes={[
-          {
-            path: AppRoute.ROOT,
-            element: <App />,
-            children: [
-              {
-                path: AppRoute.ROOT,
-                element: 'Root',
-              },
-              {
-                path: AppRoute.SIGN_IN,
-                element: <Auth />,
-              },
-              {
-                path: AppRoute.SIGN_UP,
-                element: <Auth />,
-              },
-              {
-                path: AppRoute.ANY,
-                element: <NotFound />,
-              },
-            ],
-          },
-        ]}
-      />
+      <Router />
     </StoreProvider>
   </StrictMode>,
 );
