7cb4e00107082c664d726a40664fe3d64bca7444
TetianaDoroshko <yanisolua@gmail.com>
2023-08-21 17:01:42 +0300

    th-12: + users, groups table schema

--- backend/src/libs/packages/database/schema/tables-schema.ts
+++ backend/src/libs/packages/database/schema/tables-schema.ts
@@ -1,4 +1,5 @@
 import {
+  integer,
   pgTable,
   serial,
   timestamp,
@@ -11,11 +12,16 @@ const users = pgTable(
   {
     id: serial('id').primaryKey(),
     phone: varchar('phone').notNull(),
+    email: varchar('email'),
+    firstName: varchar('first_name'),
+    lastName: varchar('last_name'),
     passwordHash: varchar('password_hash').notNull(),
     passwordSalt: varchar('password_salt').notNull(),
+    groupId: integer('group_id').references(() => groups.id),
     createdAt: timestamp('created_at').notNull().defaultNow(),
     updatedAt: timestamp('updated_at').notNull().defaultNow(),
   },
+
   (users) => {
     return {
       phoneIdx: uniqueIndex('users_phone_unique_idx').on(users.phone),
@@ -23,4 +29,12 @@ const users = pgTable(
   },
 );
 
-export { users };
+const groups = pgTable('groups', {
+  id: serial('id').primaryKey(),
+  name: varchar('name').notNull(),
+  key: varchar('key').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at').notNull().defaultNow(),
+});
+
+export { groups, users };
