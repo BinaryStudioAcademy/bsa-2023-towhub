5a7ea9de7e2a8c5eb330ceff8a23968d4dcd88d6
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-21 21:22:45 +0300

    th-25: + add new route Any

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -11,6 +11,7 @@ import {
 import { AppRoute } from '~/libs/enums/enums.js';
 import { store } from '~/libs/packages/store/store.js';
 import { Auth } from '~/pages/auth/auth.js';
+import { NotFound } from '~/pages/not-found/not-found.js';
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
   <StrictMode>
@@ -32,6 +33,10 @@ createRoot(document.querySelector('#root') as HTMLElement).render(
               {
                 path: AppRoute.SIGN_UP,
                 element: <Auth />,
+              },
+              {
+                path: AppRoute.ANY,
+                element: <NotFound />,
               },
             ],
           },
