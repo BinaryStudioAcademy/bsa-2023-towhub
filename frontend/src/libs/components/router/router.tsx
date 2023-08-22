930b1582245a779e94f0039ad6e8ed8d151e7449
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-22 20:38:23 +0300

    th-25: * updated path for App and RouterProvider

--- frontend/src/libs/components/router/router.tsx
+++ frontend/src/libs/components/router/router.tsx
@@ -1,7 +1,9 @@
-import { App, RouterProvider } from '~/libs/components/components.js';
 import { AppRoute } from '~/libs/enums/enums.js';
 import { Auth } from '~/pages/auth/auth.js';
 import { NotFound } from '~/pages/not-found/not-found.js';
+
+import { App } from '../app/app.js';
+import { RouterProvider } from '../router-provider/router-provider.js';
 
 const Router = (): JSX.Element => (
   <RouterProvider
