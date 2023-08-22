8a8a4400bfa24eb5fe825ffe1c3ed8138e5239a9
h0wter <91832203+h0wter@users.noreply.github.com>
2023-08-22 16:26:06 +0300

    th-20: + styled button component

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -1,11 +1,27 @@
+import styles from './button.module.scss';
+
 type Properties = {
   label: string;
   type?: 'button' | 'submit';
+  size?: 'sm' | 'md';
+  variant?: 'contained' | 'outlined' | 'text';
+  disabled?: boolean;
 };
 
 const Button: React.FC<Properties> = ({
   type = 'button',
+  size = 'md',
+  variant = 'contained',
   label,
-}: Properties) => <button type={type}>{label}</button>;
+  disabled = false,
+}: Properties) => (
+  <button
+    className={[styles.btn, styles[size], styles[variant]].join(' ')}
+    type={type}
+    disabled={disabled}
+  >
+    {label}
+  </button>
+);
 
 export { Button };
