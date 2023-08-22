4e61f298d6e08f4501b57da526d22de1ed945ac7
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-22 19:47:10 +0300

    th-25: * change the path to file

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,8 +3,7 @@ import '~/assets/css/styles.scss';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { StoreProvider } from '~/libs/components/components.js';
-import { Router } from '~/libs/components/router/router.js';
+import { Router, StoreProvider } from '~/libs/components/components.js';
 import { store } from '~/libs/packages/store/store.js';
 
 createRoot(document.querySelector('#root') as HTMLElement).render(
