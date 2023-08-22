d1a4f4530bff55c6b0b4d3d8c8673db3d7a51d3b
Denys Redkin <redkindo@gmail.com>
2023-08-22 17:29:18 +0300

    th-26: + transition options

--- frontend/src/libs/packages/notification/libs/options/notification.options.default.ts
+++ frontend/src/libs/packages/notification/libs/options/notification.options.default.ts
@@ -1,17 +1,23 @@
-import { type Theme, type ToastPosition, toast } from 'react-toastify';
+import {
+  type ToastPosition,
+  type ToastTransition,
+  toast,
+} from 'react-toastify';
 
-import { theme } from '../enums/notification.theme.enum.js';
+import { NotificationTheme, NotificationTransition } from '../enums/enums.js';
 
 type IToastOptions = {
   position?: ToastPosition;
-  theme?: Theme;
+  theme?: (typeof NotificationTheme)[keyof typeof NotificationTheme];
   autoClose?: number;
+  transition?: ToastTransition;
 };
 
 const DEFAULT_NOTIFICATION_OPTIONS: IToastOptions = {
   position: toast.POSITION.TOP_RIGHT,
-  theme: theme.LIGHT,
+  theme: NotificationTheme.LIGHT,
   autoClose: 4000,
+  transition: NotificationTransition.SLIDE,
 };
 
 export { type IToastOptions, DEFAULT_NOTIFICATION_OPTIONS };
