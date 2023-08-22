e5d111940a52a3aaa7afce2e2e31c666af2b5fc3
TetianaDoroshko <yanisolua@gmail.com>
2023-08-22 17:11:26 +0300

    th-12: * user schema

--- backend/src/libs/packages/database/generated-schema/0001_powerful_sphinx.sql
+++ backend/src/libs/packages/database/generated-schema/0001_powerful_sphinx.sql
@@ -7,10 +7,10 @@ CREATE TABLE IF NOT EXISTS "groups" (
 );
 INSERT INTO "groups" (name, key) VALUES ('Customer', 'customer'), ('Buisness','business'), ('Driver', 'driver'); 
 --> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "email" varchar;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "first_name" varchar;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "last_name" varchar;--> statement-breakpoint
-ALTER TABLE "users" ADD COLUMN "group_id" integer;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "email" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "first_name" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "last_name" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "group_id" integer NOT NULL;--> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "users" ADD CONSTRAINT "users_group_id_groups_id_fk" FOREIGN KEY ("group_id") REFERENCES "groups"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
