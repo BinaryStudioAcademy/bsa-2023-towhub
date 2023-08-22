6ad3453f0f8037dc7c516c46d94fb5d4e20a5fcb
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-08-22 10:41:57 +0300

    th-18: * SignUpForm

--- frontend/src/pages/auth/components/sign-up-form/libs/constants.ts
+++ frontend/src/pages/auth/components/sign-up-form/libs/constants.ts
@@ -1,7 +1,10 @@
 import { type UserSignUpRequestDto } from '~/packages/users/users.js';
 
 const DEFAULT_SIGN_UP_PAYLOAD: UserSignUpRequestDto = {
-  phone: '',
+  name: '',
+  surname: '',
+  email: '',
+  phone: '+380',
   password: '',
 };
 
--- frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
+++ frontend/src/pages/auth/components/sign-up-form/sign-up-form.tsx
@@ -1,5 +1,7 @@
-import { Button, Input } from '~/libs/components/components.js';
-import { useAppForm, useCallback } from '~/libs/hooks/hooks.js';
+import { Link } from '~/libs/components/components.js';
+import { Form } from '~/libs/components/form/form.js';
+import { AppRoute } from '~/libs/enums/app-route.enum.js';
+import { type FormField } from '~/libs/types/form.type.js';
 import {
   type UserSignUpRequestDto,
   userSignUpValidationSchema,
@@ -12,44 +14,53 @@ type Properties = {
 };
 
 const SignUpForm: React.FC<Properties> = ({ onSubmit }: Properties) => {
-  const { control, errors, handleSubmit } = useAppForm<UserSignUpRequestDto>({
-    defaultValues: DEFAULT_SIGN_UP_PAYLOAD,
-    validationSchema: userSignUpValidationSchema,
-  });
-
-  const handleFormSubmit = useCallback(
-    (event_: React.BaseSyntheticEvent): void => {
-      void handleSubmit(onSubmit)(event_);
-    },
-    [handleSubmit, onSubmit],
-  );
+  const nameField: FormField<UserSignUpRequestDto> = {
+    label: 'Name',
+    placeholder: 'Enter your name',
+    name: 'name',
+  };
+  const surnameField: FormField<UserSignUpRequestDto> = {
+    label: 'Surname',
+    placeholder: 'Enter your surname',
+    name: 'surname',
+  };
+  const emailField: FormField<UserSignUpRequestDto> = {
+    type: 'email',
+    label: 'Email',
+    placeholder: 'Enter your email',
+    name: 'email',
+  };
+  const phoneField: FormField<UserSignUpRequestDto> = {
+    label: 'Phone',
+    placeholder: 'Enter your phone',
+    name: 'phone',
+  };
+  const passwordField: FormField<UserSignUpRequestDto> = {
+    type: 'password',
+    label: 'Password',
+    placeholder: 'Enter your password',
+    name: 'password',
+  };
 
   return (
     <>
       <h3>Sign Up</h3>
-      <form onSubmit={handleFormSubmit}>
-        <p>
-          <Input
-            type="text"
-            label="Phone"
-            placeholder="Enter your email"
-            name="phone"
-            control={control}
-            errors={errors}
-          />
-        </p>
-        <p>
-          <Input
-            type="text"
-            label="Password"
-            placeholder="Enter your password"
-            name="password"
-            control={control}
-            errors={errors}
-          />
-        </p>
-        <Button type="submit" label="Sign up" />
-      </form>
+      <Form
+        defaultValues={DEFAULT_SIGN_UP_PAYLOAD}
+        validationSchema={userSignUpValidationSchema}
+        onSubmit={onSubmit}
+        btnLabel="Create Account"
+        fields={[
+          nameField,
+          surnameField,
+          emailField,
+          phoneField,
+          passwordField,
+        ]}
+      />
+      <p>
+        Already have an account? Go to <Link to={AppRoute.SIGN_IN}>Log in</Link>
+      </p>
     </>
   );
 };
