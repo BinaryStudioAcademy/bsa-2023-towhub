51989621d9f068383d1ae03b390e54d13c4f201d
dmytró voronkov <dmvr2107@gmail.com>
2023-08-23 12:21:42 +0300

    th-48: + react-select

--- frontend/src/libs/components/app/app.tsx
+++ frontend/src/libs/components/app/app.tsx
@@ -9,6 +9,8 @@ import {
 } from '~/libs/hooks/hooks.js';
 import { actions as userActions } from '~/slices/users/users.js';
 
+import { Dropdown } from '../dropdown/dropdown.js';
+
 const App: React.FC = () => {
   const { pathname } = useLocation();
   const dispatch = useAppDispatch();
@@ -49,6 +51,7 @@ const App: React.FC = () => {
         <>
           <h2>Users:</h2>
           <h3>Status: {dataStatus}</h3>
+          <Dropdown />
           <ul>
             {users.map((it) => (
               <li key={it.id}>{it.phone}</li>
--- frontend/src/libs/components/dropdown/dropdown.tsx
+++ frontend/src/libs/components/dropdown/dropdown.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Select from 'react-select';
 
 import styles from './styles.module.scss';
 
@@ -10,19 +11,29 @@ type Properties = {
   options: Option[];
 };
 
-const Dropdown: React.FC<Properties> = ({ id, name, options }): JSX.Element => {
+const Dropdown/*: React.FC<Properties>*/ = (): JSX.Element => {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
   return (
-    <div className={styles.dropdown}>
-      <div className={`${styles['select-wrapper']}`}>
-        <select className={styles.select} name={name} id={id}>
-          {options.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.name}
-            </option>
-          ))}
-        </select>
-      </div>
-    </div>
+    // <div className={styles.dropdown}>
+    //   <div className={`${styles['select-wrapper']}`}>
+    //     <select className={styles.select} name={name} id={id}>
+    //       {options.map((option) => (
+    //         <option key={option.value} value={option.value}>
+    //           {option.name}
+    //         </option>
+    //       ))}
+    //     </select>
+    //   </div>
+    // </div>
+
+    <Select
+      options={options}
+    />
   );
 };
 
