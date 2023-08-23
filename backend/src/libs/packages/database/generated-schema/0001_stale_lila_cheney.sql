c8a03b4e7ab5313c472ddabf62bf6ef8b117d6be
albsbz <alexkamens.d@gmail.com>
2023-08-23 14:59:00 +0300

    th-50: * fixed schema

---
+++ backend/src/libs/packages/database/generated-schema/0001_stale_lila_cheney.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "users" ADD COLUMN "first_name" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "last_name" varchar NOT NULL;--> statement-breakpoint
+ALTER TABLE "users" ADD COLUMN "email" varchar NOT NULL;
