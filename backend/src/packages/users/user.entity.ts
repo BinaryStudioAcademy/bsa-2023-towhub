276cafff3148423e17f73e4085c08e52adb58e4a
TetianaDoroshko <95139041+TetianaDoroshko@users.noreply.github.com>
2023-08-23 16:09:19 +0300

    th-12: Create a user schema (#42)
    * th-12: + users, groups table schema
    
    * th-12: + sign-up operation
    
    * th-12: + bcrypt module
    
    * th-12: * fix lint bugs
    
    * th-12: * fix lint bugs
    
    * th-12: * fix lint bugs
    
    * th-12: * fix lint bugs
    
    * th-12: * fix lint bugs
    
    * th-12: + http-code
    
    * th-12: * user schema
    
    * th-12: * correct the issues
    
    * th-12: * correct the default sign-up payload
    
    * th-12: * correct the issues
    
    * th-12: * correct enums, valid schema
    
    ---------
    
    Co-authored-by: Pavel Sukhinin <psykhinin@ukr.net>

--- backend/src/packages/users/user.entity.ts
+++ backend/src/packages/users/user.entity.ts
@@ -12,16 +12,32 @@ class UserEntity implements IEntity {
 
   private 'passwordSalt': string;
 
+  private 'email': string;
+
+  private 'firstName': string;
+
+  private 'lastName': string;
+
+  private 'groupId': number;
+
   private constructor({
     id,
     phone,
     passwordHash,
     passwordSalt,
+    email,
+    firstName,
+    lastName,
+    groupId,
   }: NullableProperties<UserEntityT, 'id'>) {
     this.id = id;
     this.phone = phone;
     this.passwordHash = passwordHash;
     this.passwordSalt = passwordSalt;
+    this.email = email;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.groupId = groupId;
   }
 
   public static initialize({
@@ -29,15 +45,20 @@ class UserEntity implements IEntity {
     phone,
     passwordHash,
     passwordSalt,
-  }: Pick<
-    UserEntityT,
-    'id' | 'passwordHash' | 'passwordSalt' | 'phone'
-  >): UserEntity {
+    email,
+    firstName,
+    lastName,
+    groupId,
+  }: UserEntityT): UserEntity {
     return new UserEntity({
       id,
       phone,
       passwordHash,
       passwordSalt,
+      email,
+      firstName,
+      lastName,
+      groupId,
     });
   }
 
@@ -45,30 +66,45 @@ class UserEntity implements IEntity {
     phone,
     passwordHash,
     passwordSalt,
-  }: Pick<UserEntityT, 'passwordHash' | 'passwordSalt' | 'phone'>): UserEntity {
+    email,
+    firstName,
+    lastName,
+    groupId,
+  }: Omit<UserEntityT, 'id'>): UserEntity {
     return new UserEntity({
       id: null,
       phone,
       passwordHash,
       passwordSalt,
+      email,
+      firstName,
+      lastName,
+      groupId,
     });
   }
 
-  public toObject(): Pick<UserEntityT, 'id' | 'phone'> {
+  public toObject(): Omit<
+    UserEntityT,
+    'passwordHash' | 'passwordSalt' | 'groupId'
+  > {
     return {
       id: this.id as number,
       phone: this.phone,
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
     };
   }
 
-  public toNewObject(): Pick<
-    UserEntityT,
-    'passwordHash' | 'passwordSalt' | 'phone'
-  > {
+  public toNewObject(): Omit<UserEntityT, 'id'> {
     return {
       phone: this.phone,
       passwordHash: this.passwordHash,
       passwordSalt: this.passwordSalt,
+      email: this.email,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      groupId: this.groupId,
     };
   }
 }
--- shared/src/packages/users/libs/types/user-entity.type.ts
+++ shared/src/packages/users/libs/types/user-entity.type.ts
@@ -3,6 +3,10 @@ type UserEntity = {
   phone: string;
   passwordHash: string;
   passwordSalt: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  groupId: number;
 };
 
 export { type UserEntity };
