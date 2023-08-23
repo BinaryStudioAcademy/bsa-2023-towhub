82c6f5c08240e82db507ec9298a192877d6e69f4
Denys Redkin <redkindo@gmail.com>
2023-08-23 12:51:14 +0300

    th-55: * clsx package wrapper

--- frontend/src/libs/helpers/classname-helper.ts
+++
@@ -1,7 +0,0 @@
-import clsx from 'clsx';
-
-const getClassNames = (...classNames: string[]): string => {
-  return clsx(...classNames);
-};
-
-export { getClassNames };
--- frontend/src/libs/helpers/helpers.ts
+++ frontend/src/libs/helpers/helpers.ts
@@ -1,2 +1,2 @@
-export { getClassNames } from './classname-helper.js';
+export { getValidClassNames } from './get-valid-class-names/get-valid-class-names.helper.js';
 export { configureString } from 'shared/build/index.js';
