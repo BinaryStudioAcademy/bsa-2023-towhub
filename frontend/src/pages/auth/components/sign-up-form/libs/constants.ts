792fdad0394457d5576933a9ca1d2dd010cca951
TetianaDoroshko <yanisolua@gmail.com>
2023-08-23 14:54:41 +0300

    th-12: * correct enums, valid schema

--- frontend/src/pages/auth/components/sign-up-form/libs/constants.ts
+++ frontend/src/pages/auth/components/sign-up-form/libs/constants.ts
@@ -1,12 +1,9 @@
 import { type UserSignUpRequestDto } from '~/packages/users/users.js';
 
 const DEFAULT_SIGN_UP_PAYLOAD: UserSignUpRequestDto = {
-  name: '',
-  surname: '',
   email: '',
   phone: '+380',
   password: '',
-  email: '',
   firstName: '',
   lastName: '',
   groupId: 1,
