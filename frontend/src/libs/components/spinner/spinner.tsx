cc34008290817d010c9c5b417fad8fef77cdff07
AlexVlaso <alexvlas.javaprog@gmail.com>
2023-08-23 13:26:19 +0300

    th-46: * sinner added size

--- frontend/src/libs/components/spinner/spinner.tsx
+++ frontend/src/libs/components/spinner/spinner.tsx
@@ -2,14 +2,20 @@ import styles from './styles.module.scss';
 
 type Properties = {
   isFullScreen?: boolean;
+  size?: 'sm' | 'md' | 'lg';
 };
 
-const Spinner: React.FC<Properties> = ({ isFullScreen }: Properties) => {
+const Spinner: React.FC<Properties> = ({
+  isFullScreen,
+  size = 'sm',
+}: Properties) => {
+  const loaderClassName = `${styles.loader} ${styles[size]}`;
+
   if (isFullScreen) {
     return (
       <div className={styles.wrapper}>
         <div className={styles.container}>
-          <div className={styles.loader}>Loading...</div>
+          <div className={loaderClassName}>Loading...</div>
         </div>
       </div>
     );
@@ -17,7 +23,7 @@ const Spinner: React.FC<Properties> = ({ isFullScreen }: Properties) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.loader}>Loading...</div>
+      <div className={loaderClassName}>Loading...</div>
     </div>
   );
 };
