d686609a8d4d5c7dd1caac4c219170dbc8a0a2e0
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-24 16:20:45 +0300

    th-17: * updated imports, name component, classes

--- frontend/src/pages/welcome/welcome.tsx
+++ frontend/src/pages/welcome/welcome.tsx
@@ -1,23 +1,20 @@
-import '../../assets/css/typography.scss';
-
-import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 import { BusinessCard, CustomerCard } from '~/libs/components/components.js';
 import { AppRoute } from '~/libs/enums/enums.js';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
+import { useCallback, useNavigate } from '~/libs/hooks/hooks.js';
 
 import styles from './styles.module.scss';
 
-const Welcome: React.FC = () => {
+const WelcomePage: React.FC = () => {
   const navigate = useNavigate();
 
   const handleClick = useCallback(() => {
-    navigate(`${AppRoute.SIGN_UP}`);
+    navigate(AppRoute.SIGN_UP);
   }, [navigate]);
 
   return (
     <section className={styles.section}>
-      <h2 className={`h4 ${styles.title}`}>
+      <h2 className={getValidClassNames('h4', styles.title)}>
         Welcome to the <span className={styles.accent}>TowHub</span>! Let&apos;s
         get to know each other better
       </h2>
@@ -30,4 +27,4 @@ const Welcome: React.FC = () => {
   );
 };
 
-export { Welcome };
+export { WelcomePage };
