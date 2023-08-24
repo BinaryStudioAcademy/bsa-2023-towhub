d44e82d5c10235e86d3668362f4af9307aba02c8
Mariia Akymenko <dafnarianna@gmail.com>
2023-08-24 16:24:07 +0300

    th-17: * updated layuot, classes

--- frontend/src/libs/components/business-card/business-card.tsx
+++ frontend/src/libs/components/business-card/business-card.tsx
@@ -1,4 +1,5 @@
-import '~/assets/css/typography.scss';
+import rocket from '~/assets/img/welcome-page/rocket.png';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
 
 import { Button } from '../components.js';
 import styles from './styles.module.scss';
@@ -11,8 +12,14 @@ const BusinessCard: React.FC<Properties> = ({ onClick }: Properties) => {
   return (
     <li className={styles.card}>
       <div>
-        <h3 className={`h4 ${styles.title}`}>I am an entrepreneur</h3>
-        <p className={`text-sm ${styles.text}`}>
+        <img src={rocket} alt="rocket" className={styles.image} />
+      </div>
+
+      <div>
+        <h3 className={getValidClassNames('h4', styles.title)}>
+          I am an entrepreneur
+        </h3>
+        <p className={getValidClassNames('text-sm', styles.text)}>
           You can easily add your service to our platform and get your first
           customers in just a few minutes.
         </p>
