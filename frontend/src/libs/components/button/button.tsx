18642233255084f6e73192a581a71eed94fe09a2
h0wter <91832203+h0wter@users.noreply.github.com>
2023-08-25 15:35:48 +0300

    th-20: + adds props for width and ability to add custom classes

--- frontend/src/libs/components/button/button.tsx
+++ frontend/src/libs/components/button/button.tsx
@@ -6,36 +6,48 @@ import { Icon } from '../components.js';
 import styles from './styles.module.scss';
 
 type Properties = {
+  className?: Parameters<typeof getValidClassNames>[0];
   label: string;
   type?: 'button' | 'submit';
   size?: 'sm' | 'md';
   variant?: 'contained' | 'outlined' | 'text';
   isDisabled?: boolean;
+  isFullWidth?: boolean;
   frontIcon?: ValueOf<typeof IconName>;
   backIcon?: ValueOf<typeof IconName>;
   onClick?: () => void;
 };
 
 const Button: React.FC<Properties> = ({
+  className,
   type = 'button',
   size = 'md',
   variant = 'contained',
   label,
   isDisabled = false,
+  isFullWidth = false,
   frontIcon,
   backIcon,
   onClick,
-}: Properties) => (
-  <button
-    className={getValidClassNames(styles.btn, styles[size], styles[variant])}
-    type={type}
-    disabled={isDisabled}
-    onClick={onClick}
-  >
-    {frontIcon && <Icon iconName={frontIcon} />}
-    {label}
-    {backIcon && <Icon iconName={backIcon} />}
-  </button>
-);
+}: Properties) => {
+  return (
+    <button
+      className={getValidClassNames(
+        styles.btn,
+        styles[size],
+        styles[variant],
+        isFullWidth ? styles.fullWidth : null,
+        className,
+      )}
+      type={type}
+      disabled={isDisabled}
+      onClick={onClick}
+    >
+      {frontIcon && <Icon iconName={frontIcon} />}
+      {label}
+      {backIcon && <Icon iconName={backIcon} />}
+    </button>
+  );
+};
 
 export { Button };
