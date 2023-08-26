c01c938c47217a2bdd6eced6d90f9083fa1eb8a3
albsbz <alexkamens.d@gmail.com>
2023-08-26 22:28:24 +0300

    th-82: * fixed styles

--- frontend/src/libs/components/badge/badge.tsx
+++ frontend/src/libs/components/badge/badge.tsx
@@ -13,22 +13,28 @@ const defaultColor = 'green';
 
 type Properties = {
   children: string;
+  className?: string;
   color?: (typeof PaletteColors)[number];
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 const Badge: React.FC<Properties> = ({
   children,
+  className,
   color = defaultColor,
   onClick: handleClick,
 }: Properties): JSX.Element => {
+  const isEvent = Boolean(handleClick);
   const isColorInPalette = PaletteColors.includes(color);
   const backgroundColor = isColorInPalette ? color : defaultColor;
   const isBackgroundDark = Boolean(darkColors[backgroundColor]);
+  const isClassName = Boolean(className);
   const badgeStyles = [
     styles.badge,
     `background-${backgroundColor}`,
     isBackgroundDark ? styles.textColorLight : styles.textColorDark,
+    isEvent && styles.pointer,
+    isClassName && className,
   ];
 
   return (
--- frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
+++ frontend/src/pages/auth/components/sign-in-form/sign-in-form.tsx
@@ -1,4 +1,3 @@
-// import { Badge, Button } from '~/libs/components/components.js';
 import { Button } from '~/libs/components/components.js';
 
 type Properties = {
@@ -8,23 +7,6 @@ type Properties = {
 const SignInForm: React.FC<Properties> = () => (
   <>
     <h1>Sign In</h1>
-    {/* <Badge>{'free'}</Badge>
-    <Badge color="red-dark" onClick={handleClick}>
-      {'free'}
-    </Badge>
-    <div>
-      <Badge color="blue-dark">{'free'}</Badge>
-      <Badge color="black">{'free'}</Badge>
-      <Badge color="grey">{'free'}</Badge>
-      <Badge color="red">{'free'}</Badge>
-      <Badge color="blue">{'free'}</Badge>
-      <Badge color="white">{'free'}</Badge>
-      <Badge color="grey-dark">{'free'}</Badge>
-      <Badge color="red-light">{'free'}</Badge>
-      <Badge color="blue-light">{'free'}</Badge>
-      <Badge color="grey-light">{'free'}</Badge>
-      <Badge color="grey-extra-light">{'free'}</Badge>
-    </div> */}
     <form>
       <Button label="Sign in" />
     </form>
