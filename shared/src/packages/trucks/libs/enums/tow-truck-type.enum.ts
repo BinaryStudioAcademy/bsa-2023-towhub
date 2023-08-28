a410f6702fc4d6bf3353cc92733069ab28dabc56
Denys Redkin <redkindo@gmail.com>
2023-08-28 17:22:05 +0300

    th-87: + tow truck type

---
+++ shared/src/packages/trucks/libs/enums/tow-truck-type.enum.ts
@@ -0,0 +1,8 @@
+const TowTruckType = {
+  FLATBED_OR_ROLLBACK: 'flatbed_or_rollback',
+  HOOK_AND_CHAIN: 'hook_and_chain',
+  INTEGRATED: 'integrated',
+  WHEEL_LIFT: 'wheel_lift',
+} as const;
+
+export { TowTruckType };
