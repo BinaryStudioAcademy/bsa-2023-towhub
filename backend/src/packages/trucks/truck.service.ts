a20bf23c79db7ecd021b256644e449b1d62372a6
Denys Redkin <redkindo@gmail.com>
2023-08-30 13:56:16 +0300

    th-87: + truck service

---
+++ backend/src/packages/trucks/truck.service.ts
@@ -0,0 +1,48 @@
+import { type IService } from '~/libs/interfaces/service.interface';
+
+import {
+  type TruckAddRequestDto,
+  type TruckEntityDatabase,
+} from './libs/types/types.js';
+import { TruckEntity } from './truck.entity.js';
+import { type TruckRepository } from './truck.repository.js';
+
+class TruckService implements IService {
+  private repository: TruckRepository;
+
+  public constructor(repository: TruckRepository) {
+    this.repository = repository;
+  }
+
+  public async find(id: number): Promise<TruckEntityDatabase | null> {
+    return await this.repository.find(id);
+  }
+
+  public async create(
+    payload: TruckAddRequestDto,
+  ): Promise<TruckEntityDatabase> {
+    const newTruckEntity = TruckEntity.initializeNew({
+      manufacturer: payload.manufacturer.value,
+      year: payload.year.value,
+      towType: payload.towType.value,
+      capacity: payload.capacity,
+      pricePerKm: payload.pricePerKm,
+      licensePlateNumber: payload.licensePlateNumber,
+    });
+
+    return await this.repository.create(newTruckEntity.toObject());
+  }
+
+  public async update(parameters: {
+    id: number;
+    payload: Partial<TruckEntityDatabase>;
+  }): Promise<TruckEntityDatabase> {
+    return await this.repository.update(parameters);
+  }
+
+  public async delete(id: number): Promise<boolean> {
+    return await this.repository.delete(id);
+  }
+}
+
+export { TruckService };
