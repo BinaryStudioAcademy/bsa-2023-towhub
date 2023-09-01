b938203b0e50496e034eea2b1808d0efb45f6fbe
albsbz <alexkamens.d@gmail.com>
2023-09-01 10:35:13 +0300

    th-127: + tax number accepts digits

--- shared/src/packages/users/libs/validation-schemas/enums/user-validation-rule.enum.ts
+++ shared/src/packages/users/libs/validation-schemas/enums/user-validation-rule.enum.ts
@@ -3,7 +3,7 @@ const UserValidationRule = {
   NAME: /^['A-Za-z-]{1,40}$/,
   PHONE: /^\+\d{8,19}$/,
   TAX_NUMBER: /^[\d\s./A-Z-]{8,19}$/,
-  COMPANY_NAME: /^[\s!#&'*+,.;?@A-Z_a-z~-]{1,40}$/,
+  COMPANY_NAME: /^[\s\w!#&'*+,.;?@~-]{1,40}$/,
   EMAIL_MIN_LENGTH: 5,
   EMAIL_MAX_LENGTH: 254,
 } as const;
