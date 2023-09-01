3091bd98f00b98f21168f9fc9f9e765d745d4238
albsbz <alexkamens.d@gmail.com>
2023-09-01 11:17:32 +0300

    th-128: + space support in name

--- shared/src/packages/users/libs/validation-schemas/enums/user-validation-rule.enum.ts
+++ shared/src/packages/users/libs/validation-schemas/enums/user-validation-rule.enum.ts
@@ -1,6 +1,6 @@
 const UserValidationRule = {
   PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)[\dA-Za-z]{6,20}$/,
-  NAME: /^['A-Za-z-]{1,40}$/,
+  NAME: /^[A-Za-z][\s'A-Za-z-]{0,39}$/,
   PHONE: /^\+\d{8,19}$/,
   TAX_NUMBER: /^[\d\s./A-Z-]{8,19}$/,
   COMPANY_NAME: /^[\s!#&'*+,.;?@A-Z_a-z~-]{1,40}$/,
