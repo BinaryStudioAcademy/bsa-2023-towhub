140fc2d366476befe74b6afa998141e7cb431fb9
TetianaDoroshko <yanisolua@gmail.com>
2023-09-01 09:59:02 +0300

    th-85: * correct issues

--- shared/src/packages/users/libs/types/business-sign-up-response-dto.type.ts
+++ shared/src/packages/users/libs/types/business-sign-up-response-dto.type.ts
@@ -3,11 +3,6 @@ import { type BusinessEntityT } from '~/packages/business/business.js';
 import { type UserEntityT } from './user-entity.type.js';
 import { type UserGroupEntityObjectT } from './user-group-entity.type.js';
 
-// import { type UserGroupEntityT } from './user-group-entity.type.js';
-
-// // TODO: refactor the type!
-// type GroupEntityT = { id: number; name: string; key: string };
-
 type BusinessSignUpResponseDto = Omit<
   UserEntityT,
   'passwordHash' | 'passwordSalt' | 'groupId'
--- shared/src/packages/users/libs/types/customer-sign-up-response-dto.type.ts
+++ shared/src/packages/users/libs/types/customer-sign-up-response-dto.type.ts
@@ -1,11 +1,6 @@
 import { type UserEntityT } from './user-entity.type.js';
 import { type UserGroupEntityObjectT } from './user-group-entity.type.js';
 
-// import { type UserGroupEntityT } from './user-group-entity.type.js';
-
-// TODO: refactor the type!
-// type GroupEntityT = { id: number; name: string; key: string };
-
 type CustomerSignUpResponseDto = Omit<
   UserEntityT,
   'passwordHash' | 'passwordSalt' | 'groupId'
