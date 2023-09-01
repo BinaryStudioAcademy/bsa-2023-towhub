9f99cfc798ee35968c8857aff024cc36bf6bcc13
Max Hopaytsa <gopayts2@gmail.com>
2023-09-01 13:18:34 +0300

    th-88: * linting

--- backend/src/libs/packages/database/generated-schema/0004_lame_miracleman.sql
+++ backend/src/libs/packages/database/generated-schema/0004_lame_miracleman.sql
@@ -3,7 +3,7 @@ ADD COLUMN "business_id" integer NOT NULL;
 
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "driver_details" ADD CONSTRAINT "driver_details_business_id_business_details_id_fk" FOREIGN KEY ("business_id") REFERENCES "business_details"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "driver_details" ADD CONSTRAINT "driver_details_business_id_business_details_id_fk" FOREIGN KEY ("business_id") REFERENCES "business_details"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
- WHEN duplicate_object THEN null;
+WHEN duplicate_object THEN null;
 END $$;
