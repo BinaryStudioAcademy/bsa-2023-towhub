95171edc3b7d5de87b271c8713d46637c873a545
Some14u <some1@email.ua>
2023-09-02 16:26:15 +0300

    th-90: * prettify some types

--- frontend/src/libs/types/async-thunk-config.type.ts
+++ frontend/src/libs/types/async-thunk-config.type.ts
@@ -1,7 +1,11 @@
-import { type AppDispatch, type store } from '~/libs/packages/store/store.js';
+import {
+  type AppDispatch,
+  type RootState,
+  type store,
+} from '~/libs/packages/store/store.js';
 
 type AsyncThunkConfig = {
-  state: ReturnType<typeof store.instance.getState>;
+  state: RootState;
   dispatch: AppDispatch;
   extra: typeof store.extraArguments;
 };
