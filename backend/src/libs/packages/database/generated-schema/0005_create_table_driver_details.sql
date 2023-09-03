b3cd69b7746512f3b99a9eb3fce1ea3ea427c374
Max Hopaytsa <gopayts2@gmail.com>
2023-09-03 13:48:17 +0300

    th-88: * linting

--- backend/src/libs/packages/database/generated-schema/0005_create_table_driver_details.sql
+++ backend/src/libs/packages/database/generated-schema/0005_create_table_driver_details.sql
@@ -11,14 +11,14 @@ CREATE TABLE IF NOT EXISTS
 
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "driver_details" ADD CONSTRAINT "driver_details_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "driver_details" ADD CONSTRAINT "driver_details_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
- WHEN duplicate_object THEN null;
+WHEN duplicate_object THEN null;
 END $$;
 
 --> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "driver_details" ADD CONSTRAINT "driver_details_business_id_business_details_id_fk" FOREIGN KEY ("business_id") REFERENCES "business_details"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "driver_details" ADD CONSTRAINT "driver_details_business_id_business_details_id_fk" FOREIGN KEY ("business_id") REFERENCES "business_details"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
- WHEN duplicate_object THEN null;
+WHEN duplicate_object THEN null;
 END $$;
