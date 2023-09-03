2c8c1ed27cd44a47dc29de9f05bc7895706ee388
Denys Redkin <redkindo@gmail.com>
2023-09-03 13:06:10 +0300

    th-87: * remove storage

--- frontend/src/packages/trucks/trucks-api.ts
+++ frontend/src/packages/trucks/trucks-api.ts
@@ -1,36 +1,45 @@
+import { HttpMethod } from 'shared/build/libs/packages/http/libs/enums/http-method.enum.js';
+
 import { ApiPath, ContentType } from '~/libs/enums/enums.js';
-import { HttpApi } from '~/libs/packages/api/api.js';
-import { type IHttp } from '~/libs/packages/http/http.js';
-import { type IStorage } from '~/libs/packages/storage/storage.js';
+import { HttpHeader } from '~/libs/packages/http/http.js';
+import { type Http } from '~/libs/packages/http/http.package.js';
+import { type ValueOf } from '~/libs/types/types.js';
 
 import { TruckApiPath } from './libs/enums/enums.js';
 import { type TruckEntity } from './libs/types/types.js';
 
-type Constructor = {
-  baseUrl: string;
-  http: IHttp;
-  storage: IStorage;
-};
+class TruckApi {
+  private baseUrl: string;
 
-class TruckApi extends HttpApi {
-  public constructor({ baseUrl, http, storage }: Constructor) {
-    super({ path: ApiPath.TRUCKS, baseUrl, http, storage });
+  private http: Http;
+
+  public constructor(baseUrl: string, http: Http) {
+    this.baseUrl = baseUrl;
+    this.http = http;
   }
 
-  public async addTruck(
-    payload: Omit<TruckEntity, 'id'>,
-  ): Promise<TruckEntity> {
-    const response = await this.load(
-      this.getFullEndpoint(TruckApiPath.ROOT, {}),
-      {
-        method: 'POST',
-        contentType: ContentType.JSON,
-        payload: JSON.stringify(payload),
-        hasAuth: false,
-      },
-    );
+  public async addTruck(truck: Omit<TruckEntity, 'id'>): Promise<TruckEntity> {
+    const path = this.getFullEndpoint(TruckApiPath.ROOT);
 
-    return await response.json<TruckEntity>();
+    const response = await this.http.load(path, {
+      method: HttpMethod.POST,
+      headers: this.getHeaders(ContentType.JSON),
+      payload: JSON.stringify(truck),
+    });
+
+    return (await response.json()) as TruckEntity;
+  }
+
+  private getHeaders(contentType: ValueOf<typeof ContentType>): Headers {
+    const headers = new Headers();
+
+    headers.append(HttpHeader.CONTENT_TYPE, contentType);
+
+    return headers;
+  }
+
+  private getFullEndpoint(path: string): string {
+    return `${this.baseUrl}${ApiPath.TRUCKS}${path}`;
   }
 }
 
