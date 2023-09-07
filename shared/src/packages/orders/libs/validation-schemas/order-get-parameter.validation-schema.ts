35025f35467e0e19aada6de0d75c42e924b1988a
albsbz <alexkamens.d@gmail.com>
2023-09-07 13:44:10 +0300

    th-110: * order get parameter schema

--- shared/src/packages/orders/libs/validation-schemas/order-get-parameter.validation-schema.ts
+++ shared/src/packages/orders/libs/validation-schemas/order-get-parameter.validation-schema.ts
@@ -1,13 +1,12 @@
 import joi from 'joi';
 
 import { type Id } from '~/libs/types/types.js';
+import { positiveRequiredIntegerSchema } from '~/libs/validation-schemas/validation-schemas.js';
 
 import { OrdersValidationMessage } from '../enums/enums.js';
 
 const orderGetParameter = joi.object<Id, true>({
-  id: joi.number().integer().positive().required().messages({
-    'number': OrdersValidationMessage.ID_MUST_BE_NUMBER,
-  }),
+  id: positiveRequiredIntegerSchema(OrdersValidationMessage.ID_MUST_BE_NUMBER),
 });
 
 export { orderGetParameter };
