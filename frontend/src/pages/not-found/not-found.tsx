9f772f756a561684127ecbcabddb02b34112f7bf
dmytró voronkov <dmvr2107@gmail.com>
2023-09-08 07:47:38 +0300

    th-166: * link changed to button

--- frontend/src/pages/not-found/not-found.tsx
+++ frontend/src/pages/not-found/not-found.tsx
@@ -1,16 +1,27 @@
-import { Link } from '~/libs/components/components.js';
+import { Button } from '~/libs/components/components.js';
 import { AppRoute } from '~/libs/enums/enums.js';
+import { useCallback, useNavigate } from '~/libs/hooks/hooks.js';
 
 import styles from './style.module.scss';
 
-const NotFound = (): JSX.Element => (
-  <div className={styles.page}>
-    <h1 className={styles.code}>404</h1>
-    <p className={styles.message}>OOPS! PAGE NOT FOUND</p>
-    <Link className={styles.link} to={AppRoute.WELCOME}>
-      GO TO HOMEPAGE
-    </Link>
-  </div>
-);
+const NotFound = (): JSX.Element => {
+  const navigate = useNavigate();
+
+  const handleClick = useCallback(() => {
+    navigate(AppRoute.WELCOME);
+  }, [navigate]);
+
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.code}>404</h1>
+      <p className={styles.message}>OOPS! PAGE NOT FOUND</p>
+      <Button
+        className={styles.link}
+        label="GO TO HOMEPAGE"
+        onClick={handleClick}
+      />
+    </div>
+  );
+};
 
 export { NotFound };
