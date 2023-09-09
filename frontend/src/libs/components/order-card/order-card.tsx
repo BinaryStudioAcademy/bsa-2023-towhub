16ff681fee9c8d21e69cd891ffe1c4c0f3c93cb6
Max Vaisov <92586731+mV22110702@users.noreply.github.com>
2023-09-09 12:27:49 +0300

    th-81: Create order card component (#95)
    * th-81: + order card
    
    * th-81: * Migrated to camelCase class names, optimized import in styles.module.css
    
    * th-81: * moved svg to ~/assets/img/order-card; fixed imports; added types for parts of order-card props
    
    * th-191: * fixed locations displacement
    
    * th-81: * Merged development

---
+++ frontend/src/libs/components/order-card/order-card.tsx
@@ -0,0 +1,123 @@
+import { PlainSvgIconName } from '~/libs/enums/enums.js';
+import { getValidClassNames } from '~/libs/helpers/helpers.js';
+
+import { PlainSvgIcon } from '../plain-svg-icon/plain-svg-icon.js';
+import {
+  type DriverProfileInfo,
+  type OrderCurrentStatus,
+  type OrderedTruckInfo,
+  type OrderInitialStatus,
+} from './libs/types/types.js';
+import styles from './styles.module.scss';
+
+type Properties = {
+  driver: DriverProfileInfo;
+  truck: OrderedTruckInfo;
+  initialStatus: OrderInitialStatus;
+  currentStatus: OrderCurrentStatus;
+};
+
+const OrderCard: React.FC<Properties> = ({
+  driver: { firstName, lastName, profileURL },
+  truck: { licensePlate },
+  initialStatus: { startLocation, endLocation },
+  currentStatus: { timespanLastUpdated, location, distanceLeft, timespanLeft },
+}: Properties) => {
+  const areManyKilometers = distanceLeft > 1;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.cardLayout}>
+        <div className={styles.horizontalBar}>
+          <div>
+            <PlainSvgIcon name={PlainSvgIconName.HORIZONTAL_BAR} />
+          </div>
+        </div>
+        <div className={styles.header}>
+          <div className={styles.headerImageContainer}>
+            <img
+              className={styles.profileImage}
+              src={profileURL}
+              alt="header"
+            />
+          </div>
+          <div className={styles.headerInfoContainer}>
+            <div className={styles.headerTitleContainer}>
+              <span className="text-md">
+                {firstName} {lastName}
+              </span>
+            </div>
+            <div className={styles.headerSubtitleContainer}>
+              <span className={getValidClassNames(styles.subtitle, 'text-sm')}>
+                {licensePlate}
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.body}>
+          <div className={styles.bodyContent}>
+            <div className={styles.locationDot}>
+              <PlainSvgIcon name={PlainSvgIconName.LOCATION_DOT} />
+            </div>
+            <span className={getValidClassNames(styles.location, 'text-sm')}>
+              {location}
+            </span>
+            <span className={getValidClassNames(styles.lastUpdate, 'text-sm')}>
+              last updated {timespanLastUpdated} ago
+            </span>
+            <div
+              className={getValidClassNames(
+                styles.routeDot,
+                styles.routeDotStart,
+              )}
+            >
+              <PlainSvgIcon name={PlainSvgIconName.BLUE_CIRCLE} />
+            </div>
+            <span
+              className={getValidClassNames(
+                styles.routeInfo,
+                styles.routeInfoStart,
+                'text-sm',
+              )}
+            >
+              {startLocation}
+            </span>
+            <div className={styles.routeArrow}>
+              <PlainSvgIcon name={PlainSvgIconName.ARROW_DOWN} />
+            </div>
+            <div
+              className={getValidClassNames(
+                styles.routeDot,
+                styles.routeDotEnd,
+              )}
+            >
+              <PlainSvgIcon name={PlainSvgIconName.RED_CIRCLE} />
+            </div>
+            <span
+              className={getValidClassNames(
+                styles.routeInfo,
+                styles.routeInfoEnd,
+              )}
+            >
+              {endLocation}
+            </span>
+            <div className={styles.distanceIcon}>
+              <PlainSvgIcon name={PlainSvgIconName.MAP} />
+            </div>
+            <span
+              className={getValidClassNames(
+                styles.distanceInfo,
+                styles.distanceInfoEnd,
+                'text-md',
+              )}
+            >
+              {distanceLeft} km{areManyKilometers && 's'}, {timespanLeft}
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { OrderCard };
---
+++ frontend/src/libs/enums/plain-svg-icon-name.enum.ts
@@ -0,0 +1,10 @@
+const PlainSvgIconName = {
+  ARROW_DOWN: 'arrow-down',
+  BLUE_CIRCLE: 'blue-circle',
+  HORIZONTAL_BAR: 'horizontal-bar',
+  LOCATION_DOT: 'location-dot',
+  MAP: 'map',
+  RED_CIRCLE: 'red-circle',
+} as const;
+
+export { PlainSvgIconName };
