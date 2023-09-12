cb03238ab1c40b68e5ebdc0b3e3580d30ea5a0c3
Max Hopaytsa <gopayts2@gmail.com>
2023-09-12 20:48:09 +0300

    th-168: * linting

--- backend/src/libs/packages/database/generated-schema/0010_add_businessId_to_trucks.sql
+++ backend/src/libs/packages/database/generated-schema/0010_add_businessId_to_trucks.sql
@@ -1,4 +1,7 @@
-ALTER TABLE "trucks" ADD COLUMN "business_id" integer NOT NULL;--> statement-breakpoint
+ALTER TABLE "trucks"
+ADD COLUMN "business_id" integer NOT NULL;
+
+--> statement-breakpoint
 DO $$ BEGIN
 ALTER TABLE "trucks" ADD CONSTRAINT "trucks_business_id_business_details_id_fk" FOREIGN KEY ("business_id") REFERENCES "business_details"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
