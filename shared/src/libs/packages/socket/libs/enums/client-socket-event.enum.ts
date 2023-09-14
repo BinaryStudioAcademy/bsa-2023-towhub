7f6955cd7e5c0c0f7be013f8858b13874c43b7de
h0wter <91832203+h0wter@users.noreply.github.com>
2023-09-14 13:24:39 +0300

    th-170: + socket events to get trucks list

--- shared/src/libs/packages/socket/libs/enums/client-socket-event.enum.ts
+++ shared/src/libs/packages/socket/libs/enums/client-socket-event.enum.ts
@@ -1,5 +1,6 @@
 const ClientSocketEvent = {
   CONNECT: 'connect',
+  GET_TRUCKS_LIST_REQUEST: 'get_trucks_list_request',
 } as const;
 
 export { ClientSocketEvent };
--- shared/src/libs/packages/socket/libs/enums/server-socket-event.enum.ts
+++ shared/src/libs/packages/socket/libs/enums/server-socket-event.enum.ts
@@ -2,6 +2,7 @@ const ServerSocketEvent = {
   CONNECTION: 'connection',
   DISCONNECT: 'disconnect',
   DRIVER_LOCATION_UPDATE: 'driver_location_update',
+  TRUCKS_LIST_UPDATE: 'trucks_list_update',
 } as const;
 
 export { ServerSocketEvent };
