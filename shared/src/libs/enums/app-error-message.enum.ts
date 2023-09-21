4339bcff697870eb89b25d9a8dcc002298d2e2c5
mV22110702 <92586731+mV22110702@users.noreply.github.com>
2023-09-21 10:31:26 +0300

    th-270: * Fixed comment

--- shared/src/libs/enums/app-error-message.enum.ts
+++ shared/src/libs/enums/app-error-message.enum.ts
@@ -1,7 +1,7 @@
 const AppErrorMessage = {
   INVALID_QUERY: 'Invalid SQL query was provided!',
   ENTITY_NOT_FOUND: 'Requested entity not found!',
-  ENTITY_ACCESS_DENIED: 'The user does not have access to the entity',
+  ENTITY_ACCESS_DENIED: 'The user does not have access to the entity!',
   INVALID_FILE_INPUT_CONFIG:
     'Invalid file input config was provided to file validator!',
 } as const;
