4e5d70708fc239f6a89d0b9219be11b6e6ad7929
dmytró voronkov <dmvr2107@gmail.com>
2023-09-23 16:04:49 +0300

    th-333: + style improvements

--- frontend/src/pages/driver-order/driver-order.tsx
+++ frontend/src/pages/driver-order/driver-order.tsx
@@ -7,6 +7,22 @@ import { LoadScript } from '~/libs/types/types.js';
 
 import styles from './styles.module.scss';
 
+// TODO: REMOVE MOCK
+const TRIP_INFO = {
+  distance: 10,
+  time: 30,
+  price: 40,
+};
+
+const ORDER_DETAILS = {
+  customerName: 'Ann',
+  phone: '+380676319541',
+  time: '15:20',
+  location: 'Address 1',
+  destination: 'Address 2',
+  cars: 1,
+};
+
 const DriverOrder = (): JSX.Element => {
   return (
     <section className={styles.page}>
@@ -14,64 +30,53 @@ const DriverOrder = (): JSX.Element => {
         <div className={styles['trip-info']}>
           <p className={styles.header}>TRIP INFO</p>
           <div className={styles['trip-info-content']}>
-            <span>Distance</span>
-            <span>Time</span>
-            <span>Price</span>
+            <span className={styles.item}>
+              Distance: <span>{TRIP_INFO.distance}km</span>
+            </span>
+            <span className={styles.item}>
+              Time: <span>{TRIP_INFO.time} minutes</span>
+            </span>
+            <span className={styles.item}>
+              Price: <span>${TRIP_INFO.price}</span>
+            </span>
           </div>
         </div>
         <div className={styles['order-details']}>
           <p className={styles.header}>ORDER DETAILS</p>
           <div>
             <p className={styles.detail}>
-              <Icon
-                className={getValidClassNames(styles.icon, styles['user-icon'])}
-                iconName="user"
-              />{' '}
-              Customer name: <span className={styles.value}>Ann</span>
+              <Icon className={styles['user-icon']} iconName="user" /> Customer
+              name:{' '}
+              <span className={styles.value}>{ORDER_DETAILS.customerName}</span>
+            </p>
+            <p className={styles.detail}>
+              <Icon className={styles['phone-icon']} iconName="phone" /> Phone:{' '}
+              <span className={styles.value}>{ORDER_DETAILS.phone}</span>
+            </p>
+            <p className={styles.detail}>
+              <Icon className={styles['time-icon']} iconName="clock" /> Time:{' '}
+              <span className={styles.value}>{ORDER_DETAILS.time}</span>
             </p>
             <p className={styles.detail}>
               <Icon
-                className={getValidClassNames(
-                  styles.icon,
-                  styles['phone-icon'],
-                )}
-                iconName="phone"
-              />{' '}
-              Phone: <span className={styles.value}>+380676319541</span>
-            </p>
-            <p className={styles.detail}>
-              <Icon
-                className={getValidClassNames(styles.icon, styles['time-icon'])}
-                iconName="clock"
-              />{' '}
-              Time: <span className={styles.value}>21.07 15:30</span>
-            </p>
-            <p className={styles.detail}>
-              <Icon
-                className={getValidClassNames(
-                  styles.icon,
-                  styles['location-icon'],
-                )}
+                className={styles['location-icon']}
                 iconName="location dot"
               />{' '}
-              Location: <span className={styles.value}>Address</span>
+              Location:{' '}
+              <span className={styles.value}>{ORDER_DETAILS.location}</span>
             </p>
             <p className={styles.detail}>
               <Icon
-                className={getValidClassNames(
-                  styles.icon,
-                  styles['destination-icon'],
-                )}
+                className={styles['destination-icon']}
                 iconName="location dot"
               />{' '}
-              Destination: <span className={styles.value}>Address</span>
+              Destination:{' '}
+              <span className={styles.value}>{ORDER_DETAILS.destination}</span>
             </p>
             <p className={styles.detail}>
-              <Icon
-                className={getValidClassNames(styles.icon, styles['car-icon'])}
-                iconName="car"
-              />{' '}
-              Cars need to be towed: <span className={styles.value}>1</span>
+              <Icon className={styles['car-icon']} iconName="car" /> Cars need
+              to be towed:{' '}
+              <span className={styles.value}>{ORDER_DETAILS.cars}</span>
             </p>
           </div>
           <p className={styles['comment-header']}>Comment: </p>
