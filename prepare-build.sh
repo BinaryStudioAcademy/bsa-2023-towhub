6c87710dec1d22c1c5c8774a917f36290be43476
Denys Redkin <redkindo@gmail.com>
2023-09-25 12:07:27 +0300

    th-239: * fix bug with mail views

--- prepare-build.sh
+++ prepare-build.sh
@@ -11,6 +11,19 @@ copy_and_clean() {
   fi
 }
 
+copy_files() {
+  source_dir="$1"
+  destination_dir="$2"
+
+  if [ -d "$source_dir" ]; then
+    mkdir -p "$destination_dir"
+    cp -r "$source_dir"/* "$destination_dir"
+  fi
+}
+
+# Copy mail layouts
+copy_files "./backend/src/packages/mail/libs/views/layouts" "./build/backend/packages/mail/libs/views/layouts"
+
 copy_and_clean "./backend/build" "./build/backend/"
 mkdir -p "./build/shared/"
 cp -r "./shared/build" "./build/shared/"
@@ -19,3 +32,5 @@ copy_and_clean "./frontend/build" "./build/backend/public"
 
 # Copy package files
 cp "package.json" "package-lock.json" "./build/"
+
+
