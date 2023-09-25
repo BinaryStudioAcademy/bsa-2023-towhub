69796ef655c3513411ff79fbfeeba2907344d8e2
dmytró voronkov <dmvr2107@gmail.com>
2023-09-25 13:58:47 +0300

    th-333: * validation message fix

--- shared/src/packages/orders/libs/enums/orders-validation-message.enum.ts
+++ shared/src/packages/orders/libs/enums/orders-validation-message.enum.ts
@@ -7,7 +7,7 @@ const OrdersValidationMessage = {
   CARS_QTY_MUST_BE_NUMBER: 'Orders carsQty must be number',
   FIELD_IS_REQUIRED: 'This field is mandatory',
   NAME_NOT_VALID: 'Must be 1 - 40 characters, start with a letter, no digits',
-  PHONE_NOT_VALID: 'Must be 7-18 digits, start with +',
+  PHONE_NOT_VALID: 'Must be 7 - 18 digits, start with +',
 } as const;
 
 export { OrdersValidationMessage };
