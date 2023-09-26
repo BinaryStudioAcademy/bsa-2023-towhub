571f84ba25157ac0b09da801e96f28e7f955d12a
albsbz <alexkamens.d@gmail.com>
2023-09-26 15:20:38 +0300

    th-369: * map render on reload

--- frontend/src/pages/homepage/homepage.tsx
+++ frontend/src/pages/homepage/homepage.tsx
@@ -1,25 +1,26 @@
-import { type Libraries, LoadScript } from '@react-google-maps/api';
-
-import { Map } from '~/libs/components/map/map.js';
 import {
+  useAppMap,
   useAppSelector,
   useEffect,
   useHomePageSocketService,
-  useMemo,
+  useRef,
 } from '~/libs/hooks/hooks.js';
-import { config } from '~/libs/packages/config/config.js';
 import { selectTrucks } from '~/slices/trucks/selectors.js';
 
 import { TruckList } from './components/truck-list/truck-list.js';
-import { getTruckLocations } from './libs/helpers/get-truck-locations.helper.js';
 import styles from './styles.module.scss';
-
-const libraries: Libraries = ['places'];
 
 const HomePage: React.FC = () => {
   const trucks = useAppSelector(selectTrucks);
 
-  const truckMarkers = useMemo(() => getTruckLocations(trucks), [trucks]);
+  const mapReference = useRef<HTMLDivElement>(null);
+
+  useAppMap({
+    center: null,
+    destination: null,
+    className: styles.map,
+    mapReference: mapReference,
+  });
 
   const { connectToHomeRoom, disconnectFromHomeRoom } =
     useHomePageSocketService();
@@ -33,19 +34,14 @@ const HomePage: React.FC = () => {
   }, [connectToHomeRoom, disconnectFromHomeRoom]);
 
   return (
-    <LoadScript
-      googleMapsApiKey={config.ENV.API.GOOGLE_MAPS_API_KEY}
-      libraries={libraries}
-    >
-      <div className={styles.container}>
-        <section className={styles.trucks}>
-          <TruckList trucks={trucks} />
-        </section>
-        <section className={styles.map}>
-          <Map className={styles['map-component']} markers={truckMarkers} />
-        </section>
-      </div>
-    </LoadScript>
+    <div className={styles.container}>
+      <section className={styles.trucks}>
+        <TruckList trucks={trucks} />
+      </section>
+      <section className={styles.map}>
+        <div ref={mapReference} id="map" className={styles.map} />
+      </section>
+    </div>
   );
 };
 
