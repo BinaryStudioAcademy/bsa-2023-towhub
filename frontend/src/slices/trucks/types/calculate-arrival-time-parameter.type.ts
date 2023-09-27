02a826ae30f60be7960132597ec7e630dbfdeb8d
Max Hopaytsa <gopayts2@gmail.com>
2023-09-27 14:31:22 +0300

    th-373: * linting

--- frontend/src/slices/trucks/types/calculate-arrival-time-parameter.type.ts
+++ frontend/src/slices/trucks/types/calculate-arrival-time-parameter.type.ts
@@ -2,7 +2,7 @@ import { type TruckLocationPayload } from './truck-location-payload.type.js';
 
 type CalculateArrivalTimeParameter = {
   origin: TruckLocationPayload;
-  destination: string;
+  destination: google.maps.LatLngLiteral;
 };
 
 export { type CalculateArrivalTimeParameter };
