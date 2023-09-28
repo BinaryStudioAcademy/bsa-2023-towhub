d0c064ccd97cdb39d0712a78777a61b493ff10f2
Vladyslav Holik <vladyslavholik@gmail.com>
2023-09-28 16:43:05 +0300

    th-349: + adds dynamic redirect by clicking on the logo (#382)
    Co-authored-by: Alina Kupchyk <61375322+kalinkaaaa14@users.noreply.github.com>

--- frontend/src/libs/components/header/header.tsx
+++ frontend/src/libs/components/header/header.tsx
@@ -1,10 +1,11 @@
 import { AppRoute } from '~/libs/enums/enums.js';
 import { getValidClassNames } from '~/libs/helpers/helpers.js';
-import { useCallback, useNavigate } from '~/libs/hooks/hooks.js';
+import { useCallback, useMemo, useNavigate } from '~/libs/hooks/hooks.js';
 import { useAuthUser } from '~/slices/auth/auth.js';
 
 import { AppLogo, BurgerMenu, Button, Link } from '../components.js';
 import { getBurgerMenuItems } from './libs/helpers/helpers.js';
+import { userGroupToRedirectLink } from './libs/maps/maps.js';
 import styles from './styles.module.scss';
 
 const Header: React.FC = () => {
@@ -13,6 +14,11 @@ const Header: React.FC = () => {
   const navigate = useNavigate();
 
   const burgerItems = getBurgerMenuItems(user?.group.key ?? null);
+  const navigateTo = useMemo(() => {
+    return user?.group
+      ? userGroupToRedirectLink[user.group.key]
+      : AppRoute.ROOT;
+  }, [user]);
 
   const handleSignIn = useCallback(() => {
     navigate(AppRoute.SIGN_IN);
@@ -25,7 +31,7 @@ const Header: React.FC = () => {
   return (
     <header className={styles.container}>
       <div className={styles.content}>
-        <Link className={styles.logoContainer} to={AppRoute.ROOT}>
+        <Link className={styles.logoContainer} to={navigateTo}>
           <AppLogo />
         </Link>
         <div className={styles.navMenu}>
