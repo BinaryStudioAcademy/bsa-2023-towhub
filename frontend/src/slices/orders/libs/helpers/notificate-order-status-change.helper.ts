9326af26a6320434ffade0b823f0c6bd7174026c
dmytró voronkov <dmvr2107@gmail.com>
2023-09-28 15:26:20 +0300

    th-333: * fix

--- frontend/src/slices/orders/libs/helpers/notificate-order-status-change.helper.ts
+++ frontend/src/slices/orders/libs/helpers/notificate-order-status-change.helper.ts
@@ -1,23 +1,24 @@
 import { type OrderStatusValues } from 'shared/build';
 
+import { OrderStatus } from '~/libs/enums/enums.js';
 import { notification } from '~/libs/packages/notification/notification.js';
 
 const notificateOrderStatusChange = (status: OrderStatusValues): void => {
   switch (status) {
-    case 'confirmed': {
+    case OrderStatus.CONFIRMED: {
       notification.success('Order Accepted');
       break;
     }
-    case 'picking_up': {
+    case OrderStatus.PICKING_UP: {
       notification.success('Order Confirmed');
       break;
     }
-    case 'done': {
+    case OrderStatus.DONE: {
       notification.success('Order completed');
       break;
     }
-    case 'rejected':
-    case 'canceled': {
+    case OrderStatus.REJECTED:
+    case OrderStatus.CANCELED: {
       notification.warning('Order canceled');
       break;
     }
