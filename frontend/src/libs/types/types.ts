b9957e4e681d0f0f3dadccbe4a6242633f79d715
h0wter <91832203+h0wter@users.noreply.github.com>
2023-09-29 10:52:39 +0300

    th-216: - removes unused types

--- frontend/src/libs/types/types.ts
+++ frontend/src/libs/types/types.ts
@@ -9,7 +9,6 @@ export { type SelectOption } from './select-option.type.js';
 export { type ServerErrorHandling } from './server-error-handling.type.js';
 export { type TabName, type TabsType } from './sidebar.type.js';
 export { type TruckFilters } from './truck-filters.type.js';
-export { type Libraries, LoadScript } from '@react-google-maps/api';
 export { type ColumnDef } from '@tanstack/react-table';
 export { type DeepPartial, type FieldValues } from 'react-hook-form';
 export {
